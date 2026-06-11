260735b6e5b4e9196403b838cbfaadd476ccd2a1
daniyilamelin <maksimkotok28@gmail.com>
2026-06-11 20:35:58 +0300

    Change STATIC_URL to 'app/static/'

--- app/app/settings.py
+++ app/app/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'app/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 LOGIN_REDIRECT_URL = '/'
 AUTH_USER_MODEL = 'main.User'
