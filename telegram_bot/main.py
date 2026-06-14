681b3a2b34e62e4dc3e4610e1ce0d2e429e422f7
daniyilamelin <maksimkotok28@gmail.com>
2026-06-14 12:23:54 +0300

    Refactor bot to use webhook instead of polling

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,22 +1,29 @@
-import asyncio
-from aiogram import Dispatcher, Bot
-from handlers import music_router
-import os
+from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-
-bot = Bot(token = os.getenv("BOT_TOKEN"))
+WEBHOOK_HOST = os.getenv("WEBHOOK_HOST") 
+WEBHOOK_PATH = f"/webhook/{os.getenv('BOT_TOKEN')}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 dp = Dispatcher()
 
-async def main():
+async def on_startup(app):
     dp.include_router(music_router)
-    await dp.start_polling(bot)
+    await bot.set_webhook(WEBHOOK_URL)
 
+async def on_shutdown(app):
+    await bot.delete_webhook()
+
+async def main():
+    app = web.Application()
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+    
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    
+    return app
 
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("\n🛑 Зупинено користувачем")
-    except Exception as e:
-        print(f"\n❌ Помилка: {e}")
+    app = asyncio.run(main())
+    web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
